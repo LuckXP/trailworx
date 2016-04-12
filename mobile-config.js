2caae53b1cebdbf200d2e868648ac882eff69222
Jay Cheroske <jay@portal.org>
2016-04-12 15:02:53 -0600

    added some meteor packages. Created mobile config file

---
+++ mobile-config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+App.info({
+  id: 'app.trail-worx',
+  name: 'TrailWorx',
+  description: 'Fix trails, build community!',
+  author: 'TrailWorx, LTD',
+  email: 'contact@trailworx.com',
+  website: 'http://trailworx.com'
+});
+
+App.accessRule('*.google.com/*');
+App.accessRule('*.googleapis.com/*');
+App.accessRule('*.gstatic.com/*');
+
