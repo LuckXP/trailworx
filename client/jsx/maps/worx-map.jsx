e46fa280dbd8255deaf4832c70e2241c73a18b28
cristinefolz <cristinefolz@gmail.com>
2016-04-13 19:00:26 -0600

    createContainer fix

--- client/jsx/maps/worx-map.jsx
+++ client/jsx/maps/worx-map.jsx
@@ -8,24 +8,26 @@ import { GoogleMap, Marker } from 'react-google-maps'
 import MapLoader from './map-loader'
 
 const mapMeteorToProps = (props) => {
-  return {
-    currentLocation: Geolocation.latLng()
-  }
+  const currentLocation = Geolocation.latLng();
+  const lat = currentLocation ? currentLocation.lat : 0;
+  const lng = currentLocation ? currentLocation.lng : 0;
+  return {lat, lng};
 }
 
 const WorxMap = (props) => {
-  const { currentLocation } = props
+  const { lat, lng } = props
   return (
     <MapLoader>
       <GoogleMap
         defaultZoom={3}
-        defaultCenter={{ lat: currentLocation.lat, lng: currentLocation.lng }}
+        defaultCenter={{ lat: 0, lng: 0 }}
+        center={{ lat, lng }}
         options={{ styles: mapStyles }}
       >
-        <Marker position={{ lat: currentLocation.lat, lng: currentLocation.lng }} onClick={() => alert('hello world')} />
+        <Marker position={{ lat, lng }} onClick={() => alert('hello world')} />
       </GoogleMap>
     </MapLoader>
   )
 }
 
-export default createContainer(mapMeteorToProps)(WorxMap);
+export default createContainer(mapMeteorToProps, WorxMap);
