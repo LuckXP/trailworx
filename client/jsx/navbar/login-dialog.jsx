5874ffe4e4294a67bc72cbc0f43524adaec83917
LuckXP <js.sindler@gmail.com>
2016-04-22 13:24:43 -0600

    dialog box auto close

--- client/jsx/navbar/login-dialog.jsx
+++ client/jsx/navbar/login-dialog.jsx
@@ -4,18 +4,36 @@ import FlatButton from 'material-ui/FlatButton'
 import {Meteor} from 'meteor/meteor'
 
 
-export default class extends React.Component {
+class Component extends React.Component {
   constructor(props) {
     super(props);
   }
+  loginCallBack(err) {
+    console.log("called", this)
+    const {displayNotification} = this.context;
+    if(err) {
+      console.log(err);
+      displayNotification('You could not be logged in.');
+    } else {
+      displayNotification('You logged in.');
+    }
+    this.props.closeLoginDialog();
 
+  }
   render() {
     return (
       <Dialog open={this.props.open} actions={ <FlatButton label="close" onClick={ this.props.closeLoginDialog }/> }>
-        <FlatButton label="Facebook" onClick={ () => Meteor.loginWithFacebook() }/>
-        <FlatButton label="Google" onClick={ () => Meteor.loginWithGoogle() }/>
-        <FlatButton label="Twitter" onClick={ () => Meteor.loginWithTwitter() }/>
+        <FlatButton label="Facebook" onClick={ () => Meteor.loginWithFacebook( (err) => this.loginCallBack(err) ) }/>
+        <FlatButton label="Google" onClick={ () => Meteor.loginWithGoogle( (err) => this.loginCallBack(err) ) }/>
+        <FlatButton label="Twitter" onClick={ () => Meteor.loginWithTwitter( (err) => this.loginCallBack(err) ) }/>
       </Dialog>
     )
   }
-}
+};
+
+Component.contextTypes = {
+  displayNotification: React.PropTypes.func.isRequired
+};
+
+export default Component;
+
